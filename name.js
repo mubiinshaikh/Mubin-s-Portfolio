3f89c95d4ef1368280646221f16ae32dcfc9b77d
Mubin Shaikh <mubinshaikh@Mubins-MacBook-Air.local>
2023-08-19 17:45:53 +0530

    first commit

---
+++ name.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings: ["Engineer","Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    cursorChar: ' |',
+    smartBackspace: true,
+    loop: true
+});
